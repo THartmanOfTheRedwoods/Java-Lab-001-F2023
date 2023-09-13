8709c0b8405b28336578a428be4e2409a30a3beb
aprice539 <anakinprice69@gmail.com>
2023-09-13 13:06:29 -0700

    fixed the xwing art pew pew

--- src/HelloXwing.java
+++ src/HelloXwing.java
@@ -28,6 +28,8 @@ public class HelloXwing {
     }
 
     public static void main(String[] args) {
-        // Comment
+        HelloXwing.xwing();
+        System.out.println(HelloXwing.xwing());
+        System.out.println("pew pew");
     }
 }
